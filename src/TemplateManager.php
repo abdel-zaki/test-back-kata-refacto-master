82cf6f41759398cb1380ed7d0786d9e9368df63f
Abdelwahed ZAKI <abdel1109@gmail.com>
2019-06-20 02:29:46 +0200

    Utiliser la classe TemplateAnnex

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -1,17 +1,22 @@
 <?php
 
+require_once __DIR__ . '/../src/TemplateAnnex.php';
+
 class TemplateManager
 {
     /** @const */
-    private const SUMMARY_HTML = '[quote:summary_html]';
-    /** @const */
-    private const SUMMARY = '[quote:summary]';
-    /** @const */
-    private const DESTINATION_NAME = '[quote:destination_name]';
-    /** @const */
-    private const DESTINATION_LINK = '[quote:destination_link]';
-    /** @const */
     private const FIRST_NAME = '[user:first_name]';
+    /** @var */
+    private $tempAnnex;
+
+    /**
+     * TemplateManager constructor.
+     */
+    public function __construct()
+    {
+        $this->tempAnnex = new TemplateAnnex();
+    }
+
     /**
      * @param Template $tpl
      * @param array $data
@@ -37,7 +42,7 @@ class TemplateManager
      */
     private function computeSubject($text, array $data)
     {
-        return $this->computeQuotes($text, $data);
+        return $this->tempAnnex->computeQuotes($text, $data);
     }
 
     /**
@@ -47,7 +52,7 @@ class TemplateManager
      */
     private function computeContent($text, array $data)
     {
-        $text = $this->computeQuotes($text, $data);
+        $text = $this->tempAnnex->computeQuotes($text, $data);
         /*
          * USER
          * remplacer le placeholder [user:first_name]
@@ -60,53 +65,8 @@ class TemplateManager
             $_user = ApplicationContext::getInstance()->getCurrentUser();
         }
         if ($_user) {
-            $text = $this->replaceQuotes(self::FIRST_NAME, ucfirst(mb_strtolower($_user->firstname)), $text);
+            $text = $this->tempAnnex->replaceQuotes(self::FIRST_NAME, ucfirst(mb_strtolower($_user->firstname)), $text);
         }
-
-        return $text;
-    }
-
-    /**
-     * @param String $text
-     * @param array $data
-     * @return string
-     */
-    private function replaceQuotes($quotes, $replacement, $text)
-    {
-        return str_replace($quotes, $replacement, $text);
-    }
-
-    /**
-     * @param String $text
-     * @param array $data
-     * @return string
-     */
-    private function computeQuotes($text, array $data)
-    {
-        $quote = null;
-        if (isset($data['quote']) and $data['quote'] instanceof Quote) {
-            $quote = $data['quote'];
-        }
-
-        /*
-         * remplacer les placeholders [quote:*]
-         */
-        if ($quote)
-        {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
-            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-            $placeholders = [self::SUMMARY_HTML, self::SUMMARY, self::DESTINATION_NAME];
-            $informations = [Quote::renderHtml($_quoteFromRepository), Quote::renderText($_quoteFromRepository), $destinationOfQuote->countryName];
-            $text = $this->replaceQuotes($placeholders, $informations, $text);
-        }
-
-        $destination = '';
-        if (isset($destinationOfQuote)) {
-            $destination = $usefulObject->url . '/' . $destinationOfQuote->countryName . '/quote/' . $_quoteFromRepository->id;
-        }
-        $text = $this->replaceQuotes(self::DESTINATION_LINK, $destination, $text);
 
         return $text;
     }
